8ca659f2ce4a3fcf3fdb05e1782f2b86f6e79862
wzh <zhihui258456zhihui@163.com>
2023-11-24 11:26:45 +0800

    登录页

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,20 +2,22 @@
   <div class="flex justify-center h-full gap-24">
     <div class="bg-[#495057] w-1/3 min-w-[200px] flex items-center justify-center flex-col left-icon">
       <div class="h-28 w-28">
-        <img src="../assets/images/sign.png"/>
+        <img src="../assets/images/logo.png"/>
       </div>
-      <!-- <div class="w-28 text-center text-[#FFFFFF]">
-        <img src="../assets/images/sign_text.png"/>
-      </div> -->
     </div>
-    <div class="bg-[#FFFFFF] w-1/3 px-12 py-16 relative flex items-center right-form">
-      <div class="w-80 absolute top-16 left-12">
-        <img src="../assets/images/sign_text.png"/>
+    <div class="bg-[#FFFFFF] w-1/3 px-12 py-32 flex items-stretch justify-between right-form flex-col">
+      <div class="flex h-16 items-center">
+        <div class="h-16 w-24">
+          <img src="../assets/images/logo.png"/>
+        </div>
+        <div class="">
+          <img src="../assets/images/logo_text.png"/>
+        </div>
       </div>
-      <div class="flex flex-col flex-1 justify-between gap-20 form">
+      <div class="flex flex-col justify-start gap-12 form">
         <el-input v-model="form.name" placeholder="请输入用户名" size="large"/>
         <el-input v-model="form.pw" placeholder="请输入密码" size="large"/>
-        <el-button size="large" class="w-full button" color="#495057" @click="handleClick">登录</el-button>
+        <el-button size="large" class="button mt-10" color="#495057" @click="handleClick">登录</el-button>
       </div>
     </div>
   </div> 
@@ -56,10 +58,15 @@ const handleClick = async function () {
 }
 .button {
   padding: 1.8rem 0;
+  font-size: 1rem;
 }
 
 .form >>> .el-input {
   /* --el-input-focus-border-color: #495057; */
   --el-input-focus-border-color: rgb(204,196,3);
+}
+.form >>> .el-input__wrapper {
+  font-size: 1rem;
+  padding: 0.4rem 1rem;
 }
 </style>
